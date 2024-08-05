f5bfd04e16e5c732e2f65078c04fb0fa6a9034fa
mitchparker99 <mitchelljamesparker99@gmail.com>
2024-08-05 15:16:23 +1000

    update hybrid model

--- src/model_development/hybrid_model.py
+++ src/model_development/hybrid_model.py
@@ -1,4 +1,28 @@
-def hybrid_model(user_vector, product_vectors, collaborative_matrix):
-    content_sim = content_based_recommendations(user_vector, product_vectors)
-    collaborative_sim = np.dot(collaborative_matrix, user_vector.T)
-    return 0.5 * content_sim + 0.5 * collaborative_sim
+# hybrid_model.py
+from collaborative_filtering import CollaborativeFilteringModel
+from content_based_filtering import ContentBasedFilteringModel
+
+
+class HybridModel:
+    def __init__(self):
+        self.cf_model = CollaborativeFilteringModel()
+        self.cb_model = ContentBasedFilteringModel()
+
+    def train(self, user_item_matrix, item_descriptions):
+        self.cf_model.fit(user_item_matrix)
+        self.cb_model.fit(item_descriptions)
+
+    def recommend(self, user_id, item_id, user_item_matrix, top_n=10):
+        cf_recommendations = self.cf_model.recommend(user_id, user_item_matrix)
+        cb_recommendations = self.cb_model.recommend(item_id, top_n)
+        combined_recommendations = list(
+            set(cf_recommendations) | set(cb_recommendations))
+        return combined_recommendations
+
+    def save(self, file_path):
+        # Save the model (pseudo-code, use actual saving methods)
+        pass
+
+    def load(self, file_path):
+        # Load the model (pseudo-code, use actual loading methods)
+        pass
